6fd44d5491cf4486de23f4eb0c37740ce5b21127
Diplo2by <contactdarshanv@gmail.com>
2023-06-24 10:27:06 +0530

    initial setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
+import { Montserrat } from 'next/font/google'
+
+const mont = Montserrat({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={mont.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
