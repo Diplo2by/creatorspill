2317b6e31735e9b1a15a6febf2f029e9c5a2630d
Diplo2by <contactdarshanv@gmail.com>
2023-06-25 00:26:13 +0530

    hero tweaks

--- pages/components/Hero.jsx
+++ pages/components/Hero.jsx
@@ -6,7 +6,7 @@ import Image from 'next/image'
 
 const Hero = () => {
     return (
-        <div className='h-screen snap-center flex flex-col items-center'>
+        <div className='h-screen snap-center flex flex-col items-center justify-between '>
             <Navbar />
             <div id='container' className='h-screen snap-center w-[1400px] flex justify-between'>
                 <div id='left' className='w-[2/5] flex flex-col justify-center gap-5'>
@@ -14,7 +14,7 @@ const Hero = () => {
                     <div id='subtitle' className='text-[#da4ea2]'>
                         <h2 className='text-xl font-semibold'>in that order</h2>
                     </div>
-                    <p id='description' className='text-[22px] font-semibold text-gray-300 '>Talent Management Services for People that make a difference</p>
+                    <p id='description' className='text-[22px] font-semibold text-gray-300 '>Managing Talents that make a difference</p>
                 </div>
                 <div id='right' className='w-[3/5]'>
                     <div className='top-0 right-0 bottom-0 left-0 m-auto animate-anime'>
