0dce2670beb153ea7be3a393f458c0036a434816
Diplo2by <contactdarshanv@gmail.com>
2023-06-27 18:04:48 +0530

    three JS graphic

--- pages/components/Hero.jsx
+++ pages/components/Hero.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Image from 'next/image'
-
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Sphere, MeshDistortMaterial } from "@react-three/drei";
 
 
 const Hero = () => {
     return (
-        <div className='h-screen snap-center flex flex-col items-center justify-between '>
+        <div className='h-screen snap-center flex flex-col items-center justify-between overflow-hidden '>
             <Navbar />
             <div id='container' className='h-screen snap-center w-[1400px] flex justify-between'>
                 <div id='left' className='w-[2/5] flex flex-col justify-center gap-5'>
@@ -16,8 +17,22 @@ const Hero = () => {
                     </div>
                     <p id='description' className='text-[22px] font-semibold text-gray-300 '>Managing Talents that make a difference</p>
                 </div>
-                <div id='right' className='w-[3/5]'>
-                    <div className='top-0 right-0 bottom-0 left-0 m-auto animate-anime'>
+                <div id='right' className='w-[3/5] relative flex items-center'>
+                    <div className='h-[600px] w-[800px] absolute object-contain pr-10'>
+                        <Canvas camera={{ fov: 25, position: [5, 5, 5] }}>
+                            <OrbitControls enableZoom={false} />
+                            <ambientLight intensity={1} />
+                            <directionalLight position={[3, 2, 1]} />
+                            <Sphere args={[1, 100, 200]} scale={1.5}>
+                                <MeshDistortMaterial
+                                    color="#220736"
+                                    attach="material"
+                                    distort={0.5}
+                                    speed={2} />
+                            </Sphere>
+                        </Canvas>
+                    </div>
+                    <div className='object-contain top-0 right-0 bottom-0 left-0 m-auto animate-anime'>
                         <Image src='/images/game2.png' width={800} height={600} alt='Game Controller Image' />
                     </div>
                 </div>
