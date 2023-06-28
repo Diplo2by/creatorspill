fdc4879ba2270b89fde5f99d2cc7beccbf0f33d7
Diplo2by <contactdarshanv@gmail.com>
2023-06-28 11:09:56 +0530

    coming soon page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import About from "./components/About"
+import Coming from "./components/Coming"
 import Hero from "./components/Hero"
 import Work from "./components/Work"
 
 export default function Home() {
   return (
     <div className="h-screen snap-mandatory snap-y scroll-smooth overflow-y-auto text-white bg-[url('/images/bg.jpeg')] no-scrollbar">
-      <Hero />
+      {/* <Hero />
       <About />
-      <Work />
+      <Work /> */}
+      <Coming/>
     </div>
   )
 }
