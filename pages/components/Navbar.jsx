2499056b8015511494df91f785e9eb1d62e438f7
Diplo2by <contactdarshanv@gmail.com>
2023-07-03 19:20:57 +0530

    changed logo

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
-import logo from '../../public/images/logo.png'
+import logo from '../../public/images/Logo.png'
 
 const Navbar = () => {
     return (
@@ -9,7 +9,7 @@ const Navbar = () => {
             <div className='w-[1400px] flex justify-between items-center p-3 px-0 font-medium' id='container' >
                 <div className='flex text-center gap-14' id='links'>
                     <div id='logo'>
-                        <Image src={logo} alt='' height={50} width={50} />
+                        <Image src={logo} alt='' height={100} width={100} />
                     </div>
                     <ul className='flex gap-5 items-center'>
                         <li className='transition ease-in-out delay-150 hover:translate-y-1 hover:scale-110 duration-300'><Link href='/'>Home</Link></li>
--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,21 @@ import About from "./components/About"
 import Coming from "./components/Coming"
 import Hero from "./components/Hero"
 import Work from "./components/Work"
+import Footer from "./components/Footer"
 
 export default function Home() {
   return (
     <>
     <Head>
-      <title>LvL UP Creators</title>
+      <title>LvL up Creators</title>
+      <link rel='icon' href='/icons/favicon.ico'/>
     </Head>
     <div className="h-screen snap-mandatory snap-y scroll-smooth overflow-y-auto text-white bg-[url('/images/bg.jpeg')] no-scrollbar">
-      {/* <Hero />
+      <Hero />
       <About />
-      <Work /> */}
-      <Coming/>
+      <Work />
+      {/* <Coming/> */}
+      <Footer/>
     </div>
     </>
   )
