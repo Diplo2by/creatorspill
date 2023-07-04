552e6d4528a362a679cd11056b33d764aa2dea74
Diplo2by <contactdarshanv@gmail.com>
2023-07-04 17:03:35 +0530

    Work and footer is responsive now

--- pages/components/Footer.jsx
+++ pages/components/Footer.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className="flex flex-col  items-center w-screen mt-40 snap-start min-[2000px]:mt-0">
+        <footer className="flex flex-col items-center w-screen mt-40 snap-start min-[2000px]:mt-0 max-sm:hidden" id='footer'>
             <hr className='w-[80%]' />
             <div className="flex justify-center mx-auto">
                 <p className=" my-4 text-center text-gray-200 font-semibold lg:text-xl">
                     LvL up creators &copy; 2023. All rights reserved.
                 </p>
             </div>
-        </div>
+        </footer>
     )
 }
 
--- pages/components/Work.jsx
+++ pages/components/Work.jsx
@@ -4,13 +4,13 @@ import YtMember from './YtMember'
 
 const Work = () => {
   return (
-    <div className='h-screen snap-center mx-auto' id='creators'>
+    <div className='h-screen snap-center mx-auto max-sm:px-2' id='creators'>
       <div className="container my-24 md:px-6 lg:mx-auto">
         <section className="mb-32 text-center snap-end">
           <h2 className="mb-12 text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-300 to-pink-600">
             Who we work <u className="text-primary dark:text-primary-400">with</u>
           </h2>
-          <div className="w-[90%] mx-auto grid gap-7 md:grid-cols-3 lg:gap-x-12">
+          <div className="w-[90%] mx-auto grid gap-7 md:grid-cols-3 lg:gap-x-12 max-sm:grid-cols-1">
             <Member
               twitch="https://www.twitch.tv/xcaliburye/"
               name="Xcalibur Ye"
