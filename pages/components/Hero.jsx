4bd5d746af0e7204954dc8224fc8aeb81341b886
Diplo2by <contactdarshanv@gmail.com>
2023-07-07 13:52:50 +0530

    minor changes in Hero and added email button down

--- pages/components/Hero.jsx
+++ pages/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './Navbar'
 import Image from 'next/image'
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Sphere, MeshDistortMaterial, GradientTexture,GradientType } from "@react-three/drei";
+import { OrbitControls, Sphere, MeshDistortMaterial, GradientTexture, GradientType } from "@react-three/drei";
 
 
 const Hero = () => {
@@ -11,11 +11,11 @@ const Hero = () => {
             <Navbar />
             <div id='container' className='h-screen snap-center w-[1400px] flex justify-between max-sm:w-screen max-sm:mx-4 max-sm:px-6 max-sm:text-center max-sm:items-start'>
                 <div id='left' className='w-[2/5] flex flex-col justify-center gap-5'>
-                    <div className='text-[60px] font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-300 to-pink-600'>Creators, Clients and More.</div>
+                    <div className='text-[60px] font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-300 to-pink-600'>Creators, Brands and More.</div>
                     <div id='subtitle' className='text-[#da4ea2]'>
                         <div className='text-xl font-semibold max-sm:text-3xl'>in that order</div>
                     </div>
-                    <p id='description' className='text-[22px] font-semibold text-gray-300 max-sm:text-3xl '>Managing Talents that make a difference</p>
+                    <p id='description' className='text-[22px] font-semibold text-gray-300 max-sm:text-3xl '>Managing Creators that make a difference</p>
                 </div>
                 <div id='right' className='w-[3/5] relative flex items-center max-sm:hidden'>
                     <div className='h-[600px] w-[800px] absolute object-contain pr-10'>
@@ -28,7 +28,7 @@ const Hero = () => {
                                     attach="material"
                                     distort={0.5}
                                     speed={2}>
-                                    <GradientTexture stops={[0, 0.1,1]} colors={['#ED2C52', '#db2777','#d8b4fe']} size={1000} type={GradientType.Radial} />
+                                    <GradientTexture stops={[0, 0.1, 1]} colors={['#ED2C52', '#db2777', '#d8b4fe']} size={1000} type={GradientType.Radial} />
                                 </MeshDistortMaterial>
                             </Sphere>
                         </Canvas>
---
+++ pages/components/MoreDetails.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+const MoreDetails = () => {
+    return (
+        <div>
+            <div className='text-center text-xl mt-0 my-1 max-sm:text-lg max-sm:my-4'>
+                Have More Queries? <Link href={'mailto:sayhi@lvlupCreators.com'}> <span className=' text-transparent bg-clip-text bg-gradient-to-r from-purple-300 to-pink-600'>Write to us.</span>  </Link>
+            </div>
+        </div>
+    )
+}
+
+export default MoreDetails
