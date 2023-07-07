25d5cbbd3086f749fc08f3c35546f817b72e7465
Diplo2by <contactdarshanv@gmail.com>
2023-07-07 13:53:00 +0530

    logo change 2

--- pages/index.js
+++ pages/index.js
@@ -4,21 +4,23 @@ import Coming from "./components/Coming"
 import Hero from "./components/Hero"
 import Work from "./components/Work"
 import Footer from "./components/Footer"
+import MoreDetails from "./components/MoreDetails"
 
 export default function Home() {
   return (
     <>
-    <Head>
-      <title>LvL up Creators</title>
-      <link rel='icon' href='/icons/favicon.ico'/>
-    </Head>
-    <div className="h-screen snap-mandatory snap-y scroll-smooth overflow-y-auto text-white bg-[url('/images/bg.jpeg')] no-scrollbar max-sm:overflow-x-hidden">
-      <Hero />
-      <About />
-      <Work />
-      {/* <Coming/> */}
-      <Footer/>
-    </div>
+      <Head>
+        <title>LvL up | Creators, Brands & More. </title>
+        <link rel='icon' href='/icons/favicon.ico' />
+      </Head>
+      <div className="h-screen snap-mandatory snap-y scroll-smooth overflow-y-auto text-white bg-[url('/images/bg.jpeg')] no-scrollbar max-sm:overflow-x-hidden">
+        <Hero />
+        <About />
+        <Work />
+        {/* <Coming/> */}
+        <MoreDetails />
+        <Footer />
+      </div>
     </>
   )
 }
