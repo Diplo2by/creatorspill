e374e9b13d369fe2af8db4f6d217ae986f3ba63f
Diplo2by <contactdarshanv@gmail.com>
2025-08-22 23:16:42 +0530

    copyright change

--- pages/components/Footer.jsx
+++ pages/components/Footer.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Footer = () => {
-    return (
-        <div className="flex flex-col items-center w-screen snap-start" id='footer'>
-            <hr className='w-[80%]' />
-            <div className="flex justify-center mx-auto">
-                <p className=" my-4 text-center text-gray-200 font-semibold lg:text-xl">
-                    LvL up creators &copy; 2023. All rights reserved.
-                </p>
-            </div>
-        </div>
-    )
-}
+  const year = new Date().getFullYear();
 
-export default Footer
+  return (
+    <div className="flex flex-col items-center w-screen snap-start" id="footer">
+      <hr className="w-[80%]" />
+      <div className="flex justify-center mx-auto">
+        <p className=" my-4 text-center text-gray-200 font-semibold lg:text-xl">
+          LvL up creators &copy; {year}. All rights reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
